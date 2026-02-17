b836f6414077ede0b303b5d66cd99c393f2408cd
ajeetyadav7464 <ajeetyadav7464@gmail.com>
2026-02-17 23:13:34 +0530

    Update geo.cpp

--- geo.cpp
+++ geo.cpp
@@ -1,5 +1,3 @@
-dfg
-
 #define _USE_MATH_DEFINES  // Define to ensure math constants like M_PI are available
 #include "geo.h"           // Include necessary headers
 #include <cmath>           // Include cmath for mathematical functions
